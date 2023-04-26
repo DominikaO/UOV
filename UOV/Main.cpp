eeaaf9dd6ede9eb0854338561f9d7bee10b5076e
root <root@vmi1268059.contaboserver.net>
2023-04-26 18:00:19 +0200

    new run of tests stored to new_time.csv

--- UOV/Main.cpp
+++ UOV/Main.cpp
@@ -36,7 +36,7 @@ int main()
 	long seconds, nanoseconds;
 	GF2X modulus;
 	long mod;
-	double UOV,UOVp,UOVp2;
+	double UOV,UOVp,UOVp2,keygen,keygenp,ver,verp;
 	int vin,vin1,vin2;
 	cout <<"Modulo ";
 	cin >> mod;
@@ -49,9 +49,9 @@ int main()
 	Vec<GF2E> h;
 	Vec<GF2E> riesenia;
 	Vec<Vec<GF2E>> vsetky_moznosti;
-	long v = 4; long o = 3; long t = 2;
-	cout << "Insert vinegar,oil,t (example: 56 48 8): ";
-	cin >> v >> o >>t;
+	long v = 4; long o = 3; long t;
+	cout << "Insert vinegar,oil,t (example: 56 48 ): ";
+	cin >> v >> o;
 	//cout << "Generating all possible solutions for UOV pertubation v1 ..." << endl;
 	//generuj_vsetky_moznosti(vsetky_moznosti, t, mod);
 
@@ -60,20 +60,36 @@ int main()
 
 	publicKey puk;
         privateKey prk;
-
+	for (t = 1; t <= 8; t++){
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
 	KeyGen_p(pk, sk, o, v, t);
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+	seconds = end.tv_sec - begin.tv_sec;
+	nanoseconds = end.tv_nsec - begin.tv_nsec;
+	cout << "Time keygen UOVp: " << seconds + nanoseconds*1e-9 <<endl;
+	keygenp=seconds + nanoseconds*1e-9;
+	
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
 	KeyGen(puk,prk,o,v);
-	cerr << "keyGen done!" << endl;
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+	seconds = end.tv_sec - begin.tv_sec;
+	nanoseconds = end.tv_nsec - begin.tv_nsec;
+	cout << "Time keygen UOV: " << seconds + nanoseconds*1e-9 <<endl;
+	keygen=seconds + nanoseconds*1e-9;
 
 	string f;
-	cout << "Enter filename for output stats: ";
-        cin >> f;
+	//cout << "Enter filename for output stats: ";
+        //cin >> f;
+	
+	cout << "t= "<< t<<endl;
+	f = "new_time" + to_string(t) + ".csv";
 	ofstream file(f);
+
 	// Write the column headers to the file
-	file << "UOV,UOV_vinegars,UOVp,UOVp_vinegars, UOV2, UOVp2_vinegars\n";
+	file << "UOV,UOV_vinegars,UOVp,UOVp_vinegars, UOV2, UOVp2_vinegars,keygen,keygenp\n";
 
 	for (int opakovanie = 0; opakovanie < 100; opakovanie++) {
-		//cout << "Iteracia č."<< opakovanie << endl;
+		cout << "Iteracia č."<< opakovanie << endl;
 		//
 		Vec<GF2E> dokument;
 		Vec<GF2E> podpis;
@@ -103,26 +119,32 @@ int main()
 		nanoseconds = end.tv_nsec - begin.tv_nsec;
 		cout << "Time UOVp2: " << seconds + nanoseconds*1e-9 <<endl;
 		UOVp2=seconds + nanoseconds*1e-9;
-
+		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
+		verify_p(podpis,dokument,pk,o);
+		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+		verp =  ((end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec))*1e-9;
 
 		//cout << "--------------------------------------------------- UOV ---------------------------------------------------" << endl;
-	        vin = sign(podpis, prk, dokument, v, o);
+	        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
+		vin = sign(podpis, prk, dokument, v, o);
 		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
 		seconds = end.tv_sec - begin.tv_sec;
 		nanoseconds = end.tv_nsec - begin.tv_nsec;
 		cout << "Time UOV: " << seconds + nanoseconds*1e-9 << endl;
 		UOV=seconds + nanoseconds*1e-9;
-
+		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
 		verify(podpis, dokument, puk, o);	
+		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+		ver =  ((end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec))*1e-9;
 
 		// Write the data to the file
-	         file << UOV << ","<< vin << "," << UOVp<< ","<< vin1<< "," << UOVp2 << "," << vin2 << endl;
+	         file << UOV << ","<< vin << "," << UOVp<< ","<< vin1<< "," << UOVp2 << "," << vin2 << ","<< keygen << ","<< keygenp << ","<<ver<<"," << verp<< endl;
 		
 
 	
 	}
 	
 	file.close();
-
+	}
 	return 0;
 		}
